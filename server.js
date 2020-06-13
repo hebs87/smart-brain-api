b9a905a40496c945a8c745c79e51a503f811c403
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-13 21:39:05 +0100

    Created signing route for user details of first record in database object

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 const express = require('express');
 
 const app = express();
+// Converts the response body to JSON so Express can read it
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: 1,
+      name: 'Sunny',
+      email: 'sunnyhebbar@hotmail.co.uk',
+      password: 'password',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: 1,
+      name: 'Kim',
+      email: 'kimjones92@hotmail.co.uk',
+      password: 'password',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
 
 app.get('/', (req, res) => {
-  res.send('this is working');
-})
+  res.send('<h1>This is working</h1>');
+});
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password) {
+    res.json('Success');
+  } else {
+    res.status(400).json('Error logging in')
+  }
+});
 
 // Middleware that listens on port 3000
 app.listen(3000, () => {
-  console.log('app is running on port 3000');
+  console.log('App is running on port 3000');
 })
 
 /*
