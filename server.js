92cc86c13b00c1f8df66170a2ac79161107a68c7
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-14 02:57:00 +0100

    Updated image route to increment entries in the db

--- server.js
+++ server.js
@@ -101,15 +101,14 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   const {id} = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === Number(id)) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  })
-  if (!found) res.status(404).json('No such user');
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    // Returns an object
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0])
+    })
+    .catch(err => res.status(400).json('Unable to get entries'))
 });
 
 // Middleware that listens on port 5000
