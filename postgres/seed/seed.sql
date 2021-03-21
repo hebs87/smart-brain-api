ebebb34a3d80eca31bd51ec20a160eb703767d30
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-03-21 01:24:13 +0000

    Added password value comment to seed.sql

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -3,6 +3,7 @@ BEGIN TRANSACTION;
 INSERT into users (name, email, entries, joined)
 values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
 
+-- Password is a hash of 123
 INSERT into login (hash, email)
 values ('$2a$10$joqwWutyIxfucntraGDg8OujeyT3lIbro9PF9N6o6vXX.gxXKdaTu', 'jessie@gmail.com');
 
